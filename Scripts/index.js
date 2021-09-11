0082cb7874aa8bb019d7a30221e25c8adc9184c6
ragulram417 <ragulram417@gmail.com>
2021-09-11 13:40:44 +0530

    Verified

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,64 +1,58 @@
-'use strict'
+"use strict";
 
-// get elements by varibales
+// get elements by variables
 const form = document.getElementById(`form`);
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const confirmPassword = document.getElementById('confirm-password');
-
-// functions
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirm-password");
 // showError function
 function showError(input, message) {
   const formControl = input.parentElement;
-  formControl.className = 'form-control error';
-  const small = formControl.querySelector('small');
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
   small.innerText = message;
 }
-
-// showSuccess function 
+// showSuccess function
 function showSuccess(input) {
   const formControl = input.parentElement;
-  formControl.className = 'form-control success';
+  formControl.className = "form-control success";
 }
-
-// check email function
+// regex code for Email
 function checkEmail(email) {
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
-
-// eventHandler
-form.addEventListener('submit', function (e) {
+// eventHandler function
+form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  if (username.value === '') {
-    showError(username, 'Username is required');
+  if (username.value === "") {
+    showError(username, "Enter a username");
   } else {
     showSuccess(username);
   }
 
-  if (email.value === '') {
-    showError(email, 'Email is required');
-  }else if(!checkEmail(email.value)){
-    showError(email, 'Email is invalid')
-  }
-  else {
+  if (email.value === "") {
+    showError(email, "Enter a Email");
+  } else if (!checkEmail(email.value)) {
+    showError(email, "Enter a valid Email");
+  } else {
     showSuccess(email);
   }
 
-  if (password.value === '') {
-    showError(password, 'password is required');
+  if (password.value === "") {
+    showError(password, "Enter a password");
   } else {
     showSuccess(password);
   }
 
-  if (confirmPassword.value === '') {
-    showError(confirmPassword, 'Confirm Password is required');
-  } else if (password.value !== confirmPassword.value){
-    showError(confirmPassword, `Password don't match`);
-  }
-  else {
+  if (confirmPassword.value === "") {
+    showError(confirmPassword, "Please Confirm your Password");
+  } else if (password.value !== confirmPassword.value) {
+    showError(confirmPassword, `Both Passwords don't match`);
+  } else {
     showSuccess(confirmPassword);
   }
-})
+});
